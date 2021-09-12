bff874af2b7e32ec1a0bf8a1c4e9f4a1d081d0af
Varun Gautam <90300946+varun2005pro@users.noreply.github.com>
2021-09-12 19:06:38 +0530

    Add files via upload

---
+++ Assigmnet_operators.py
@@ -0,0 +1,34 @@
+#Addittion operators used to assign values to variables 👇
+x_1= 5       
+x_1 += 3
+print(x_1)     
+
+#Substraction operators used to assign values to variables 👇
+x_2= 7
+x_2 -= 2
+print(x_2)
+
+#mulitplication operators used to assign values to variables 👇
+x_3 = 8
+x_3 *= 9
+print(x_3)
+
+#Floating division operators used to assign values to variables 👇
+x_4= 5
+x_4 /= 3
+print(x_4)
+
+#modulo division operators used to assign values to variables 👇
+x_5 = 5
+x_5%=3
+print(x_5)
+
+#Integer-division division operators used to assign values to variables 👇
+x_6 = 5
+x_6//=3
+print(x_6)
+
+#Exponent division operators used to assign values to variables 👇
+x_7 = 3
+x_7 **= 7
+print(x_7)
